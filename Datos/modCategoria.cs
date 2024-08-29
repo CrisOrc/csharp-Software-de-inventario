70686b0f4111da9c1db337e79738f8b87a54e3be
unknown <cristian.camilo.or05@gmail.com>
2024-08-29 12:43:01 -0500

    Proyecto organizado para codificar

---
+++ Datos/modCategoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class modCategoria
+    {
+        private Conexion conexion = new Conexion();
+
+        SqlDataReader buffer;
+
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+    }
+}
---
+++ Datos/modCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class modCliente
+
+    {
+        private Conexion conexion = new Conexion();
+
+        SqlDataReader buffer;
+
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+    }
+}
